3896ca924667a0477addfc645d03a97a90647773
VaishnaviBhardwaj <vaishnavibhardwaj27@gmail.com>
2020-11-05 00:20:44 +0530

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import  Cards from "./Cards";
+
+import Home from "./home";
+
+
+function App() {
+ 
+  return (
+    <diV>
+    <Home/>
+   </diV>
+  
+  
+  );
+}
+
+export default App;
---
+++ my-app/src/home.js
@@ -0,0 +1,42 @@
+import React,{useState} from 'react';
+import "./timer.css";
+import Cards from"./Cards";
+import Card2 from "./Card2";
+import { ActionButton,textbtn } from './Card.Css';
+
+
+
+
+ function Home(){
+    
+     return(
+        <diV>
+  <div className="row"> 
+   <div className="column">
+     <Cards/> 
+   </div>
+   <div className="column">
+     <Card2/>
+   </div>
+   </div>
+
+
+   <div className="Content">
+       <center>
+       <input
+       className="field"
+          type="text" 
+          placeholder="                   Enter the Bid"
+        />
+        <ActionButton><h3>Bid</h3></ActionButton>
+      
+    </center>
+   </div>
+   
+   </diV>
+  
+     );
+ }
+
+
+ export default Home
